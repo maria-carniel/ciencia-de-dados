04fbd56ad7fb14747b35db8df86e867ef32c6e53
maria luiza <m.carniel@escola.pr.gov.br>
2025-09-03 19:33:11 +0000

    aula.08, correlação parte2

--- aula06.py
+++ aula06.py
@@ -5,5 +5,11 @@ import numpy as np
 np.random.seed(42)
 
 dados = pd.DataFrame({ 
-    "Orçamento_Campanha": np.random.randint(1000, 5000, size=100)
+    "Orçamento_Campanha": np.random.randint(1000, 5000, size=100),
+    "Visualizacoes_Anuncio": np.random.randint(2000, 20000, size=100),
+    "Vendas": np.random.randint(100, 20000, size=100),
+    "Cliques": np.random.randint(100, 20000, size=100)
 })
+
+# Visualizar a correlação entre cada variável
+print(dados.corr().round(4))
